b482aaed0749cd16830385970332f7c99f3ba7c5
Fatwa <fax.mp4@gmail.com>
2023-03-20 12:35:29 +0700

    push ws-fatwa

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/Fatwaff/ws-fatwa/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/whatsauth/whatsauth"
 
-	"iteung/url"
+	"github.com/Fatwaff/ws-fatwa/url"
 
 	"github.com/gofiber/fiber/v2"
 )
